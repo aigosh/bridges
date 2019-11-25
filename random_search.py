aff70162cc9a3e0b039622a09f6a4c905fae28d9
Andrey Igoshin <a.y.igoshin@tinkoff.ru>
2019-11-25 08:31:12 +0300

    fix

--- random_search.py
+++ random_search.py
@@ -1,4 +1,4 @@
-from networkx import Graph, to_numpy_array
+from networkx import Graph
 from numpy.random import randint
 from numpy import iinfo
 import numpy as np
@@ -14,14 +14,13 @@ class Random2BridgeFinder:
         self.sort = sort
 
     def search(self):
-        # self.edge_codes = self._get_initial_codes(0)
-        self.edge_codes = self._get_initial_codes(None)
+        self.edge_codes = self._get_initial_codes()
 
         self._make_forest()
         self._fill_forest_codes()
         return self.edge_codes
 
-    def _get_initial_codes(self, value=0):
+    def _get_initial_codes(self, value=None):
         nodes_count = len(self.graph)
         shape = (nodes_count, nodes_count)
         dtype = np.int64
@@ -54,25 +53,25 @@ class Random2BridgeFinder:
         self.colors.update({node: Color.BLACK})
 
     def _fill_forest_codes(self):
-        for j in reversed(range(64)):
+        for bit_position in reversed(range(64)):
             self.colors = dict()
             for i in range(len(self.graph.nodes)):
                 if self.colors.get(i) is Color.WHITE.value:
-                    self._fill_tree_codes(i, iteration=j)
+                    self._fill_tree_codes(i, bit_position=bit_position)
 
-    def _fill_tree_codes(self, node: int, parent: int = -1, iteration: int = 0):
+    def _fill_tree_codes(self, node: int, parent: int = -1, bit_position: int = 0):
         self.colors.update({node: Color.GRAY})
 
         for neighbor in self.graph.neighbors(node):
             if self.colors.get(neighbor) not in [Color.GRAY, Color.BLACK]:
-                self._fill_tree_codes(neighbor, node, iteration)
+                self._fill_tree_codes(neighbor, node, bit_position)
 
         neighbors = list(self.graph.neighbors(node))
-        numbers = np.array(list(filter(lambda x: x != None, self.edge_codes[node][neighbors])))
-        number = np.sum((numbers >> iteration) & 1) & 1
+        codes = np.array(list(filter(lambda x: x != None, self.edge_codes[node][neighbors])))
+        bit = np.sum((codes >> bit_position) & 1) & 1
 
-        initial = self.edge_codes[node][parent] if self.edge_codes[node][parent] != None else 0
-        code = np.int64(initial) + np.int64(number << iteration)
+        initial = self.edge_codes[node][parent] if self.edge_codes[node][parent] is not None else 0
+        code = np.int64(initial) + np.int64(bit << bit_position)
         self.edge_codes[node][parent] = code
         self.edge_codes[parent][node] = code
 
